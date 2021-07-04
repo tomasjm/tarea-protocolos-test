2ee0c05673800201f7d60fa2eeaedb578f59f099
Tomás Jiménez <tjimenez999@gmail.com>
2021-07-04 03:59:22 -0400

    t

--- test.cpp
+++ test.cpp
@@ -51,7 +51,7 @@ int main(int argc, char *args[])
       if(wiringPiISR(CLOCK_PIN_SEND, INT_EDGE_RISING, &cbSend) < 0){
     printf("Unable to start interrupt function\n");
   }
-    if (wiringPiISR(CLOCK_PIN_RECEIVE, INT_EDGE_FALLING, &cbReceive) < 0)
+    if (wiringPiISR(CLOCK_PIN_RECEIVE, INT_EDGE_RISING, &cbReceive) < 0)
     {
         printf("Unable to start interrupt function\n");
     }
